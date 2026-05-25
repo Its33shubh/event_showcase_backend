f873080b3dae2bd3604f77927c5b393212af9e02
Its33shubh <shubhamkakloter123@gmail.com>
2026-05-25 21:36:55 +0530

    add the cloudinary.js config file

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
@@ -5,5 +6,15 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+export const getPublicIdFromUrl = (url) => {
+  if (!url) return null;
+
+  const parts = url.split("/");
+  const fileName = parts[parts.length - 1];
+  const publicId = fileName.split(".")[0];
+
+  return `bhavnagar-events/${publicId}`;
+};
 
 export default cloudinary;
